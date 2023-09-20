0a6ed96349bb2ca2eac21dcb0fa92d00ec3b9c6e
Tony Luck <tony.luck@intel.com>
2023-09-19 18:07:52 -0700

    resctrl2: Provide show() function for the schemata file
    Core code walks the list of registered resources and displays the
    current control value for each domain according to the format for
    the domain.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl2/schemata.c
+++ fs/resctrl2/schemata.c
@@ -3,6 +3,44 @@
 
 #include "internal.h"
 
+static void show_val(struct seq_file *m, struct resctrl_resource *r, struct resctrl_domain *d,
+		     int ctrl_indx)
+{
+	unsigned long *curval = d->ctrls;
+	int size;
+
+	switch (r->schemata_fmt) {
+	default:
+	case RESCTRL_ULONG:
+		seq_printf(m, "%lu", curval[ctrl_indx]);
+		break;
+	case RESCTRL_BITMASK:
+		size = BITS_TO_LONGS(d->param);
+
+		seq_printf(m, "%*pb", d->param, &curval[ctrl_indx * size]);
+	}
+}
+
+static int schemata_seq_show(struct seq_file *m, struct resctrl_group *rg)
+{
+	struct resctrl_resource *r;
+	struct resctrl_domain *d;
+	char *sep;
+
+	for_each_resource_by_cap(r, num_alloc_ids) {
+		seq_printf(m, "%s: ", r->schemata_name);
+		sep = "";
+		list_for_each_entry(d, &r->domains, list) {
+			seq_printf(m, "%s%d=", sep, d->id);
+			show_val(m, r, d, arch_ctrl_id(rg->resctrl_ids));
+			sep = ";";
+		}
+		seq_puts(m, "\n");
+	}
+
+	return 0;
+}
+
 bool resctrl_add_schemata_file(struct kernfs_node *parent_kn)
 {
 	struct resctrl_node_info *rni, *prni;
@@ -15,6 +53,7 @@ bool resctrl_add_schemata_file(struct kernfs_node *parent_kn)
 	rni->flags = RESCTRL_LOCK_CPUS;
 	cfi = (struct core_file_info *)&rni->priv;
 	cfi->rg = (struct resctrl_group *)&prni->priv;
+	cfi->show = schemata_seq_show;
 
 	return true;
 }
