dedcdb5cbb4428fc6b258569571b3c390f165b09
Tony Luck <tony.luck@intel.com>
2023-10-18 21:57:12 -0700

    resctrl2: Enable rmdir to remove monitor groups
    Also handle recursive removal of monitor groups when removing a
    control group, or unmounting the resctrl file system.
    
    When groups are removed, tasks are moved to parent or default group.
    Make sure that the changed resctrl_ids take effect promptly.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl2/cpu.c
+++ fs/resctrl2/cpu.c
@@ -5,6 +5,35 @@
 
 #include "internal.h"
 
+/*
+ * This is safe against resctrl_sched_in() called from __switch_to()
+ * because __switch_to() is executed with interrupts disabled. A local call
+ * from update_resctrl_ids() is protected against __switch_to() because
+ * preemption is disabled.
+ */
+static void update_cpu_resctrl_ids(void *info)
+{
+	struct resctrl_group *r = info;
+
+	if (r)
+		this_cpu_write(resctrl_per_cpu_state.default_resctrl_ids, r->resctrl_ids);
+
+	/*
+	 * Re-use the context switch code, current running
+	 * task may have its own reasctrl_ids selected.
+	 */
+	resctrl_sched_in(current);
+}
+
+/*
+ * Update the resctrl_ids on all cpus in @cpu_mask.
+ * Per task resctrl_ids must have been set up before calling this function.
+ */
+void update_resctrl_ids(const struct cpumask *cpu_mask, struct resctrl_group *r)
+{
+	on_each_cpu_mask(cpu_mask, update_cpu_resctrl_ids, r, 1);
+}
+
 static int resctrl_online_cpu(unsigned int cpu)
 {
 	struct resctrl_resource *r;
