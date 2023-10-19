cf80a157cb4c3ddd8f931686c89fdd24f1f9dd7f
Tony Luck <tony.luck@intel.com>
2023-10-18 21:57:14 -0700

    resctrl2: Add/remove the "size" file
    CTRL_MON directories should include the "size" file to report the
    amount of cache that can be allocated based on schemata settings.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

---
+++ fs/resctrl2/size.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright(c) 2023 Intel Corporation. */
+
+#include "internal.h"
+
+bool resctrl_add_size_file(struct kernfs_node *parent_kn)
+{
+	struct resctrl_node_info *rni, *prni;
+	struct core_file_info *cfi;
+
+	rni = resctrl_add_file(parent_kn, "size", 0444, RESCTRL_COREFILE);
+	if (!rni)
+		return false;
+	prni = parent_kn->priv;
+	rni->flags = RESCTRL_LOCK_CPUS;
+	cfi = (struct core_file_info *)&rni->priv;
+	cfi->rg = (struct resctrl_group *)&prni->priv;
+
+	return true;
+}
+
+void resctrl_remove_size_file(struct kernfs_node *parent_kn, struct list_head *h)
+{
+	resctrl_remove_file("size", parent_kn, h);
+}
