eeb2b6c2ff1915c1551b5d5d3170bf0264bad925
Tony Luck <tony.luck@intel.com>
2023-10-18 21:57:11 -0700

    resctrl2: Implement info/last_cmd_status file
    The resctrl user interface uses writing strings to files in the
    resctrl filesystem to submit requests. When the request cannot
    be completed, a POSIX error code is returned for the write(2) system
    call. But these lack the details that many users may need to understand
    why their request could not be completed.
    
    Implement a file in the "info/" directory of the resctrl filesystem.
    After a request fails the user may read this file to get additional
    information about why the request failed.
    
    The kernel interface routines to this file: resctrl_last_cmd_clear(),
    resctrl_last_cmd_puts() and resctrl_last_cmd_printf() are exported so
    that loadable modules for resctrl subsystem can also provide helpful
    descriptions for requests passed from the core code to a module.
    
    Remove static from definition of __resctrl_add_file() as it is now needed
    outside of the kernfs.c file.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl2/info.c
+++ fs/resctrl2/info.c
@@ -9,11 +9,64 @@ static struct resctrl_group info_header = {
 	.type = DIR_INFO
 };
 
+static struct seq_buf last_cmd_status;
+static char last_cmd_status_buf[512];
+
+void resctrl_last_cmd_clear(void)
+{
+	seq_buf_clear(&last_cmd_status);
+}
+EXPORT_SYMBOL_GPL(resctrl_last_cmd_clear);
+
+void resctrl_last_cmd_puts(const char *s)
+{
+	seq_buf_puts(&last_cmd_status, s);
+}
+EXPORT_SYMBOL_GPL(resctrl_last_cmd_puts);
+
+void resctrl_last_cmd_printf(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	seq_buf_vprintf(&last_cmd_status, fmt, ap);
+	va_end(ap);
+}
+EXPORT_SYMBOL_GPL(resctrl_last_cmd_printf);
+
+static int last_cmd_status_seq_show(struct seq_file *m, void *arg)
+{
+	int len;
+
+	len = seq_buf_used(&last_cmd_status);
+	if (len)
+		seq_printf(m, "%.*s", len, last_cmd_status_buf);
+	else
+		seq_puts(m, "ok\n");
+
+	return 0;
+}
+
+static struct kernfs_ops cmd_status_ops = {
+	.seq_show = last_cmd_status_seq_show,
+};
+
 bool resctrl_add_info_dir(struct kernfs_node *parent_kn)
 {
+	struct kernfs_node *kn;
+
+	seq_buf_init(&last_cmd_status, last_cmd_status_buf,
+		     sizeof(last_cmd_status_buf));
+
 	kn_info = resctrl_add_dir(parent_kn, "info", &info_header);
 	if (!kn_info)
 		return false;
+
+	kn = __resctrl_add_file(kn_info, "last_cmd_status", 0444, &cmd_status_ops, NULL);
+	if (!kn) {
+		kernfs_remove(kn_info);
+		return false;
+	}
 
 	return true;
 }
