c51a869597484826f7fd07ee16c92c135118d391
Tony Luck <tony.luck@intel.com>
2025-07-03 16:17:45 -0700

    x86,fs/resctrl: Fill in details of events for guid 0x26696143 and 0x26557651
    These two guids describe the events supported on Clearwater Forest.
    
    The offsets in MMIO space are arranged in groups for each RMID.
    
    E.g the "energy counters for guid 0x26696143 are arranged like this:
    
            MMIO offset:0x0000 Counter for RMID 0 PMT_EVENT_ENERGY
            MMIO offset:0x0008 Counter for RMID 0 PMT_EVENT_ACTIVITY
            MMIO offset:0x0010 Counter for RMID 1 PMT_EVENT_ENERGY
            MMIO offset:0x0018 Counter for RMID 1 PMT_EVENT_ACTIVITY
            ...
            MMIO offset:0x23F0 Counter for RMID 575 PMT_EVENT_ENERGY
            MMIO offset:0x23F8 Counter for RMID 575 PMT_EVENT_ACTIVITY
    
    Define these events in the file system code and add the events
    to the event_group structures.
    
    PMT_EVENT_ENERGY and PMT_EVENT_ACTIVITY are produced in fixed point
    format. File system code must output as floating point values.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/kernel/cpu/resctrl/intel_aet.c
+++ arch/x86/kernel/cpu/resctrl/intel_aet.c
@@ -33,6 +33,20 @@ struct pkg_mmio_info {
 };
 
 /**
+ * struct pmt_event - Telemetry event.
+ * @id:		Resctrl event id.
+ * @idx:	Counter index within each per-RMID block of counters.
+ * @bin_bits:	Zero for integer valued events, else number bits in fixed-point.
+ */
+struct pmt_event {
+	enum resctrl_event_id	id;
+	unsigned int		idx;
+	unsigned int		bin_bits;
+};
+
+#define EVT(_id, _idx, _bits) { .id = _id, .idx = _idx, .bin_bits = _bits }
+
+/**
  * struct event_group - All information about a group of telemetry events.
  * @pfg:		Points to the aggregated telemetry space information
  *			within the OOBMSM driver that contains data for all
@@ -41,6 +55,8 @@ struct pkg_mmio_info {
  * @pkginfo:		Per-package MMIO addresses of telemetry regions belonging to this group.
  * @guid:		Unique number per XML description file.
  * @mmio_size:		Number of bytes of MMIO registers for this group.
+ * @num_events:		Number of events in this group.
+ * @evts:		Array of event descriptors.
  */
 struct event_group {
 	/* Data fields for additional structures to manage this group. */
@@ -51,6 +67,8 @@ struct event_group {
 	/* Remaining fields initialized from XML file. */
 	u32				guid;
 	size_t				mmio_size;
+	unsigned int			num_events;
+	struct pmt_event		evts[] __counted_by(num_events);
 };
 
 static LIST_HEAD(active_event_groups);
@@ -65,6 +83,11 @@ static LIST_HEAD(active_event_groups);
 static struct event_group energy_0x26696143 = {
 	.guid		= 0x26696143,
 	.mmio_size	= XML_MMIO_SIZE(576, 2, 3),
+	.num_events	= 2,
+	.evts				= {
+		EVT(PMT_EVENT_ENERGY, 0, 18),
+		EVT(PMT_EVENT_ACTIVITY, 1, 18),
+	}
 };
 
 /*
@@ -74,6 +97,16 @@ static struct event_group energy_0x26696143 = {
 static struct event_group perf_0x26557651 = {
 	.guid		= 0x26557651,
 	.mmio_size	= XML_MMIO_SIZE(576, 7, 3),
+	.num_events	= 7,
+	.evts				= {
+		EVT(PMT_EVENT_STALLS_LLC_HIT, 0, 0),
+		EVT(PMT_EVENT_C1_RES, 1, 0),
+		EVT(PMT_EVENT_UNHALTED_CORE_CYCLES, 2, 0),
+		EVT(PMT_EVENT_STALLS_LLC_MISS, 3, 0),
+		EVT(PMT_EVENT_AUTO_C6_RES, 4, 0),
+		EVT(PMT_EVENT_UNHALTED_REF_CYCLES, 5, 0),
+		EVT(PMT_EVENT_UOPS_RETIRED, 6, 0),
+	}
 };
 
 static struct event_group *known_energy_event_groups[] = {
@@ -177,6 +210,8 @@ static int discover_events(struct event_group *e, struct pmt_feature_group *p)
 		mmi->addrs[--pkgcounts[tr->plat_info.package_id]] = tr->addr;
 	}
 	e->pkginfo = no_free_ptr(pkginfo);
+
+	list_add(&e->list, &active_event_groups);
 
 	return 0;
 }
