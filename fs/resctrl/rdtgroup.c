72c1d39fc4cc007301f7e424bcc128a5b3835229
Tony Luck <tony.luck@intel.com>
2025-07-03 16:17:45 -0700

    fs/resctrl: Provide interface to create architecture specific debugfs area
    Architectures are constrained to just the file interfaces provided by
    the file system for each resource. This does not allow for architecture
    specific debug interfaces.
    
    Add resctrl_debugfs_mon_info_arch_mkdir() which creates a directory in the
    debugfs file system for a resource. Naming follows the layout of the
    main resctrl hierarchy:
    
    	/sys/kernel/debug/resctrl/info/{resource}_MON/{arch}
    
    The {arch} last level directory name matches the output of
    the user level "uname -m" command.
    
    Architecture code may use this directory for debug information,
    or for minor tuning of features. It must not be used for basic
    feature enabling as debugfs may not be configured/mounted on
    production systems.
    
    Suggested-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -24,6 +24,7 @@
 #include <linux/sched/task.h>
 #include <linux/slab.h>
 #include <linux/user_namespace.h>
+#include <linux/utsname.h>
 
 #include <uapi/linux/magic.h>
 
@@ -4350,6 +4351,33 @@ cleanup_mountpoint:
 	return ret;
 }
 
+static struct dentry *debugfs_resctrl_info;
+
+/*
+ * Create /sys/kernel/debug/resctrl/info/{r->name}_MON/{arch} directory
+ * by request for architecture to use for debugging or minor tuning.
+ * Basic functionality of features must not be controlled by files
+ * added to this directory as debugs may not be configured/mounted
+ * on production systems.
+ */
+struct dentry *resctrl_debugfs_mon_info_arch_mkdir(struct rdt_resource *r)
+{
+	struct dentry *moninfodir;
+	char name[32];
+
+	if (!r->mon_capable)
+		return NULL;
+
+	if (!debugfs_resctrl_info)
+		debugfs_resctrl_info = debugfs_create_dir("info", debugfs_resctrl);
+
+	sprintf(name, "%s_MON", r->name);
+
+	moninfodir = debugfs_create_dir(name, debugfs_resctrl_info);
+
+	return debugfs_create_dir(utsname()->machine, moninfodir);
+}
+
 static bool resctrl_online_domains_exist(void)
 {
 	struct rdt_resource *r;
@@ -4401,6 +4429,7 @@ void resctrl_exit(void)
 
 	debugfs_remove_recursive(debugfs_resctrl);
 	debugfs_resctrl = NULL;
+	debugfs_resctrl_info = NULL;
 	unregister_filesystem(&rdt_fs_type);
 
 	/*
