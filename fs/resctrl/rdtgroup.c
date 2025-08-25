d59c53d976b9b847d8b45f65761ddb1b974aaf96
Tony Luck <tony.luck@intel.com>
2025-08-25 10:41:20 -0700

    x86,fs/resctrl: Refactor domain_remove_cpu_mon() ready for new domain types
    All monitoring events are associated with the L3 resource.
    
    The RDT_RESOURCE_L3 resource carries a lot of state in the domain
    structures which needs to be dealt with when a domain is taken offline
    by removing the last CPU in the domain.
    
    New telemetry events will be associated with a new package scoped
    resource with new domain structures.
    
    Refactor domain_remove_cpu_mon() so all the L3 processing is separate
    from general actions of clearing the CPU bit in the mask and removing
    sub-directories from the mon_data directory.
    
    resctrl_offline_mon_domain() continues to remove domain specific
    directories and files from the "mon_data" directories, but skips the
    L3 resource specific cleanup when called for other resource types.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -4201,6 +4201,9 @@ void resctrl_offline_mon_domain(struct rdt_resource *r, struct rdt_mon_domain *d
 	if (resctrl_mounted && resctrl_arch_mon_capable())
 		rmdir_mondata_subdir_allrdtgrp(r, d);
 
+	if (r->rid != RDT_RESOURCE_L3)
+		goto out_unlock;
+
 	if (resctrl_is_mbm_enabled())
 		cancel_delayed_work(&d->mbm_over);
 	if (resctrl_is_mon_event_enabled(QOS_L3_OCCUP_EVENT_ID) && has_busy_rmid(d)) {
@@ -4217,7 +4220,7 @@ void resctrl_offline_mon_domain(struct rdt_resource *r, struct rdt_mon_domain *d
 	}
 
 	domain_destroy_mon_state(d);
-
+out_unlock:
 	mutex_unlock(&rdtgroup_mutex);
 }
 
