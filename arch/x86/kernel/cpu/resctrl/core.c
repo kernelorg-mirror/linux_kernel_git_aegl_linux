dea826888b060795af533c8e2386c465dea1eda1
Tony Luck <tony.luck@intel.com>
2025-09-12 13:43:01 -0700

    x86/resctrl: Discover hardware telemetry events
    Each CPU collects data for telemetry events that it sends to the nearest
    telemetry event aggregator either when the value of IA32_PQR_ASSOC.RMID
    changes, or when a two millisecond timer expires.
    
    The telemetry event aggregators maintain per-RMID per-event counts of
    the total seen for all the CPUs. There may be more than one set of telemetry
    event aggregators per package.
    
    There are separate sets of aggregators for each type of event, but all
    aggregators for a given type are symmetric keeping counts for the same
    set of events for the CPUs that provide data to them.
    
    Each telemetry event aggregator is responsible for a specific group of
    events. E.g. on the Intel Clearwater Forest CPU there are two types of
    aggregators. One type tracks a pair of energy related events. The other
    type tracks a subset of "perf" type events.
    
    The event counts are made available to Linux in a region of MMIO space
    for each aggregator. All details about the layout of counters in each
    aggregator MMIO region are described in XML files published by Intel and
    made available in a GitHub repository [1].
    
    The key to matching a specific telemetry aggregator to the XML file that
    describes the MMIO layout is a 32-bit value. The Linux telemetry subsystem
    refers to this as a "guid" while the XML files call it a "uniqueid".
    
    Each XML file provides the following information:
    1) Which telemetry events are included in the group.
    2) The order in which the event counters appear for each RMID.
    3) The value type of each event counter (integer or fixed-point).
    4) The number of RMIDs supported.
    5) Which additional aggregator status registers are included.
    6) The total size of the MMIO region for an aggregator.
    
    The INTEL_PMT_TELEMETRY driver enumerates support for telemetry events.
    This driver provides intel_pmt_get_regions_by_feature() to list all
    available telemetry event aggregators. The list includes the "guid",
    the base address in MMIO space for the region where the event counters
    are exposed, and the package id where the all the CPUs that report to this
    aggregator are located.
    
    Add a new Kconfig option CONFIG_X86_CPU_RESCTRL_INTEL_AET for the Intel
    specific parts of telemetry code. This depends on the INTEL_PMT_TELEMETRY
    and INTEL_TPMI drivers being built-in to the kernel for enumeration of
    telemetry features.
    
    Use INTEL_PMT_TELEMETRY's intel_pmt_get_regions_by_feature() with
    each per-RMID telemetry feature id to obtain a private copy of
    struct pmt_feature_group that contains all discovered/enumerated
    telemetry aggregator data for all event groups (known and unknown
    to resctrl) of that feature id. Further processing on this structure
    will enable all supported events in resctrl. Return the structure to
    INTEL_PMT_TELEMETRY at resctrl exit time.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://github.com/intel/Intel-PMT # [1]

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -737,6 +737,9 @@ void resctrl_arch_pre_mount(void)
 
 	if (!atomic_try_cmpxchg(&only_once, &old, 1))
 		return;
+
+	if (!intel_aet_get_events())
+		return;
 }
 
 enum {
@@ -1094,6 +1097,8 @@ late_initcall(resctrl_arch_late_init);
 
 static void __exit resctrl_arch_exit(void)
 {
+	intel_aet_exit();
+
 	cpuhp_remove_state(rdt_online);
 
 	resctrl_exit();
