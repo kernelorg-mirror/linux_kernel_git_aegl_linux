c1218dcbc1c923372c466570b2739d35f53ef60e
Tony Luck <tony.luck@intel.com>
2025-10-13 13:26:43 -0700

    x86/resctrl: Add debugfs files to show telemetry aggregator status
    Each telemetry aggregator provides three status registers at the top
    end of MMIO space after all the per-RMID per-event counters:
    
      data_loss_count: This counts the number of times that this aggregator
      failed to accumulate a counter value supplied by a CPU core.
    
      data_loss_timestamp: This is a "timestamp" from a free running
      25MHz uncore timer indicating when the most recent data loss occurred.
    
      last_update_timestamp: Another 25MHz timestamp indicating when the
      most recent counter update was successfully applied.
    
    Create files in /sys/kernel/debug/resctrl/info/PERF_PKG_MON/x86_64/
    to display the value of each of these status registers for each aggregator
    in each enabled event group. The prefix for each file name describes
    the type of aggregator, which package it is located on, and an opaque
    instance number to provide a unique file name when there are multiple
    aggregators on a package.
    
    The suffix is one of the three strings listed above. An example name is:
    
    	energy_pkg0_agg2_data_loss_count
    
    These files are removed along with all other debugfs entries by the
    call to debugfs_remove_recursive() in resctrl_exit().
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/kernel/cpu/resctrl/internal.h
+++ arch/x86/kernel/cpu/resctrl/internal.h
@@ -236,6 +236,7 @@ int intel_aet_read_event(int domid, u32 rmid, enum resctrl_event_id evtid,
 			 void *arch_priv, u64 *val);
 void intel_aet_mon_domain_setup(int cpu, int id, struct rdt_resource *r,
 				struct list_head *add_pos);
+void intel_aet_add_debugfs(void);
 #else
 static inline bool intel_aet_get_events(void) { return false; }
 static inline void __exit intel_aet_exit(void) { }
@@ -247,6 +248,7 @@ static inline int intel_aet_read_event(int domid, u32 rmid, enum resctrl_event_i
 
 static inline void intel_aet_mon_domain_setup(int cpu, int id, struct rdt_resource *r,
 					      struct list_head *add_pos) { }
+static inline void intel_aet_add_debugfs(void) { }
 #endif
 
 #endif /* _ASM_X86_RESCTRL_INTERNAL_H */
