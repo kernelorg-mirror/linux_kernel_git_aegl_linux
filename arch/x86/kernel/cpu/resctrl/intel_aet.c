e2f47dbd43e72c4d096447739a563120ce22876d
Tony Luck <tony.luck@intel.com>
2025-10-29 08:54:06 -0700

    x86,fs/resctrl: Fill in details of events for guid 0x26696143 and 0x26557651
    The Intel Clearwater Forest CPU supports two RMID-based PMT feature
    groups documented in the xml/CWF/OOBMSM/RMID-ENERGY/cwf_aggregator.xml and
    xml/CWF/OOBMSM/RMID-PERF/cwf_aggregator.xml files in the Intel PMT GIT repository
    [1].
    
    The struct pmt_feature_group provided by the INTEL_PMT_TELEMETRY driver lists the
    guid and other information for each aggregator of a given type (energy or perf)
    present on the system.
    
    resctrl has a condensed form of the XML description in struct
    event_group. An event group is enabled if the pfg field points
    to a struct pmt_feature_group.
    
    The counter offsets in MMIO space are arranged in groups for each RMID.
    
    E.g the "energy" counters for guid 0x26696143 are arranged like this:
    
            MMIO offset:0x0000 Counter for RMID 0 PMT_EVENT_ENERGY
            MMIO offset:0x0008 Counter for RMID 0 PMT_EVENT_ACTIVITY
            MMIO offset:0x0010 Counter for RMID 1 PMT_EVENT_ENERGY
            MMIO offset:0x0018 Counter for RMID 1 PMT_EVENT_ACTIVITY
            ...
            MMIO offset:0x23F0 Counter for RMID 575 PMT_EVENT_ENERGY
            MMIO offset:0x23F8 Counter for RMID 575 PMT_EVENT_ACTIVITY
    
    After all counters there are three status registers that provide
    indications of how many times an aggregator was unable to process
    event counts, the time stamp for the most recent loss of data, and
    the time stamp of the most recent successful update.
    
    	MMIO offset:0x2400 AGG_DATA_LOSS_COUNT
    	MMIO offset:0x2408 AGG_DATA_LOSS_TIMESTAMP
    	MMIO offset:0x2410 LAST_UPDATE_TIMESTAMP
    
    Define these events in the file system code and add the events
    to the event_group structures.
    
    PMT_EVENT_ENERGY and PMT_EVENT_ACTIVITY are produced in fixed point
    format. File system code must output as floating point values.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Link: https://github.com/intel/Intel-PMT # [1]

--- arch/x86/kernel/cpu/resctrl/intel_aet.c
+++ arch/x86/kernel/cpu/resctrl/intel_aet.c
@@ -13,6 +13,7 @@
 
 #include <linux/array_size.h>
 #include <linux/cleanup.h>
+#include <linux/compiler_types.h>
 #include <linux/cpu.h>
 #include <linux/err.h>
 #include <linux/init.h>
@@ -20,10 +21,26 @@
 #include <linux/intel_vsec.h>
 #include <linux/overflow.h>
 #include <linux/resctrl.h>
+#include <linux/resctrl_types.h>
 #include <linux/stddef.h>
 #include <linux/types.h>
 
 #include "internal.h"
+
+/**
+ * struct pmt_event - Telemetry event.
+ * @id:		Resctrl event id.
+ * @idx:	Counter index within each per-RMID block of counters.
+ * @bin_bits:	Zero for integer valued events, else number bits in fraction
+ *		part of fixed-point.
+ */
+struct pmt_event {
+	enum resctrl_event_id	id;
+	unsigned int		idx;
+	unsigned int		bin_bits;
+};
+
+#define EVT(_id, _idx, _bits) { .id = _id, .idx = _idx, .bin_bits = _bits }
 
 /**
  * struct event_group - All information about a group of telemetry events.
@@ -36,6 +53,9 @@
  *			Valid if the system supports the event group.
  *			NULL otherwise.
  * @guid:		Unique number per XML description file.
+ * @mmio_size:		Number of bytes of MMIO registers for this group.
+ * @num_events:		Number of events in this group.
+ * @evts:		Array of event descriptors.
  */
 struct event_group {
 	/* Data fields for additional structures to manage this group. */
@@ -44,7 +64,13 @@ struct event_group {
 
 	/* Remaining fields initialized from XML file. */
 	u32				guid;
+	size_t				mmio_size;
+	unsigned int			num_events;
+	struct pmt_event		evts[] __counted_by(num_events);
 };
+
+#define XML_MMIO_SIZE(num_rmids, num_events, num_extra_status) \
+		      (((num_rmids) * (num_events) + (num_extra_status)) * sizeof(u64))
 
 /*
  * Link: https://github.com/intel/Intel-PMT
@@ -53,6 +79,12 @@ struct event_group {
 static struct event_group energy_0x26696143 = {
 	.feature	= FEATURE_PER_RMID_ENERGY_TELEM,
 	.guid		= 0x26696143,
+	.mmio_size	= XML_MMIO_SIZE(576, 2, 3),
+	.num_events	= 2,
+	.evts		= {
+		EVT(PMT_EVENT_ENERGY, 0, 18),
+		EVT(PMT_EVENT_ACTIVITY, 1, 18),
+	}
 };
 
 /*
@@ -62,6 +94,17 @@ static struct event_group energy_0x26696143 = {
 static struct event_group perf_0x26557651 = {
 	.feature	= FEATURE_PER_RMID_PERF_TELEM,
 	.guid		= 0x26557651,
+	.mmio_size	= XML_MMIO_SIZE(576, 7, 3),
+	.num_events	= 7,
+	.evts		= {
+		EVT(PMT_EVENT_STALLS_LLC_HIT, 0, 0),
+		EVT(PMT_EVENT_C1_RES, 1, 0),
+		EVT(PMT_EVENT_UNHALTED_CORE_CYCLES, 2, 0),
+		EVT(PMT_EVENT_STALLS_LLC_MISS, 3, 0),
+		EVT(PMT_EVENT_AUTO_C6_RES, 4, 0),
+		EVT(PMT_EVENT_UNHALTED_REF_CYCLES, 5, 0),
+		EVT(PMT_EVENT_UOPS_RETIRED, 6, 0),
+	}
 };
 
 static struct event_group *known_event_groups[] = {
