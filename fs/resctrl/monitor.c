ab72533d12c3888ed3ddb8bbedda9920216de3db
Tony Luck <tony.luck@intel.com>
2025-12-03 16:19:05 -0800

    x86,fs/resctrl: Handle events that can be read from any CPU
    resctrl assumes that monitor events can only be read from a CPU in the
    cpumask_t set of each domain.  This is true for x86 events accessed with an
    MSR interface, but may not be true for other access methods such as MMIO.
    
    Introduce and use flag mon_evt::any_cpu, settable by architecture, that
    indicates there are no restrictions on which CPU can read that event.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -518,10 +518,12 @@ static int __mon_event_count(struct rdtgroup *rdtgrp, struct rmid_read *rr)
 {
 	switch (rr->r->rid) {
 	case RDT_RESOURCE_L3:
-		if (rr->hdr)
+		if (rr->hdr) {
+			WARN_ON_ONCE(rr->evt->any_cpu);
 			return __l3_mon_event_count(rdtgrp, rr);
-		else
+		} else {
 			return __l3_mon_event_count_sum(rdtgrp, rr);
+		}
 	default:
 		rr->err = -EINVAL;
 		return -EINVAL;
@@ -987,7 +989,7 @@ struct mon_evt mon_event_all[QOS_NUM_EVENTS] = {
 	},
 };
 
-void resctrl_enable_mon_event(enum resctrl_event_id eventid)
+void resctrl_enable_mon_event(enum resctrl_event_id eventid, bool any_cpu)
 {
 	if (WARN_ON_ONCE(eventid < QOS_FIRST_EVENT || eventid >= QOS_NUM_EVENTS))
 		return;
@@ -996,6 +998,7 @@ void resctrl_enable_mon_event(enum resctrl_event_id eventid)
 		return;
 	}
 
+	mon_event_all[eventid].any_cpu = any_cpu;
 	mon_event_all[eventid].enabled = true;
 }
 
